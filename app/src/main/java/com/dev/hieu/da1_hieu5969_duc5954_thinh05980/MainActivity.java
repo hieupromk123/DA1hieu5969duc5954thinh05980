03bbba28b8507b3ef82aca47e1bca55e0173b955
OngDat <metlam446@gmail.com>
2018-10-30 13:44:31 +0700

    Initial commit

---
+++ app/src/main/java/com/dev/hieu/da1_hieu5969_duc5954_thinh05980/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dev.hieu.da1_hieu5969_duc5954_thinh05980;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
